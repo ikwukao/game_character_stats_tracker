d542699bab4f0c1261d16a027624429b16ce8503
Ikwuka Okoye <ikwukao@proton.me>
2026-05-26 19:00:35 +0100

    Create the constructor

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 class GameCharacter:
-    pass
+     def __init__(self, name):
+
+        # Store character name
+        self._name = name
+
+        # Default health points
+        self._health = 100
+
+        # Default mana points
+        self._mana = 50
+
+        # Default starting level
+        self._level = 1
